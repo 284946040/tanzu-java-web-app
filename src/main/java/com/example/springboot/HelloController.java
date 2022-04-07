398f55ad62ed516fea30691a5399dd90c399225b
284946040 <284946040@qq.com>
2022-04-07 18:10:58 +0800

    0009

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu001!";
+		return "Greetings from Spring Boot + Tanzu0011!";
 	}
 
 }
